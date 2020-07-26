70d54ce30290ed6c64e48d75599a24efff872550
Duncan Ryan <duncancryan1@gmail.com>
2020-07-26 20:05:19 +0100

    added some setup to customer tests, will work on this class first

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -2,3 +2,9 @@ import unittest
 from classes.guest import Guest
 
 class TestGuest(unittest.TestCase):
+    def setUp(self):
+        self.guest_01 = Guest("Frodo Baggins", 51)
+        self.guest_02 = Guest("Samwise Gamgee", 36)
+        self.guest_03 = Guest("Meriadoc Brandybuck", 37)
+        self.guest_04 = Guest("Peregrin Took", 29)
+
