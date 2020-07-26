5243258de98dd03861bad7e941ebe4889e98b550
Duncan Ryan <duncancryan1@gmail.com>
2020-07-26 22:56:42 +0100

    added to setup in room test so that I can begin testing and coding functionality

--- tests/room_test.py
+++ tests/room_test.py
@@ -7,6 +7,18 @@ class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room_01 = Room("The Party Room")
         self.room_02 = Room("The Date Room")
+        
+        self.guest_01 = Guest("Frodo Baggins", 51)
+        self.guest_02 = Guest("Samwise Gamgee", 36)
+        self.guest_03 = Guest("Meriadoc Brandybuck", 37)
+        self.guest_04 = Guest("Peregrin Took", 29)
+
+        self.song_01 = Song("Come on Eileen", "Dexys Midnight Runners")
+        self.song_02 = Song("Mr Blue Sky", "ELO")
+        self.song_03 = Song("Bat out of Hell", "Meatloaf")
+        self.song_04 = Song("You're so Vain", "Carly Simon")
+        self.song_05 = Song("Tubthumping", "Chumbawamba")
+        self.song_06 = Song("Accidentally in Love", "Counting Crows")
 
     def test_room_has_name(self):
         name = self.room_01.name
