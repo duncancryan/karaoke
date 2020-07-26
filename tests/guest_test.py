7651b26605a5689c9ba4e0ed2497c265c64087d9
Duncan Ryan <duncancryan1@gmail.com>
2020-07-27 00:07:28 +0100

    test added to check for a wallet property in customer objects, setup adapted

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -3,10 +3,10 @@ from classes.guest import Guest
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
-        self.guest_01 = Guest("Frodo Baggins", 51)
-        self.guest_02 = Guest("Samwise Gamgee", 36)
-        self.guest_03 = Guest("Meriadoc Brandybuck", 37)
-        self.guest_04 = Guest("Peregrin Took", 29)
+        self.guest_01 = Guest("Frodo Baggins", 51, 30.50)
+        self.guest_02 = Guest("Samwise Gamgee", 36, 10.75)
+        self.guest_03 = Guest("Meriadoc Brandybuck", 37, 23.60)
+        self.guest_04 = Guest("Peregrin Took", 29, 17.85)
 
     def test_guest_has_name(self):
         name = self.guest_01.name
@@ -15,4 +15,8 @@ class TestGuest(unittest.TestCase):
     def test_guest_has_age(self):
         age = self.guest_01.age
         self.assertEqual(51, age)
+
+    def test_guest_has_wallet(self):
+        wallet = round(self.guest_02.wallet, 2)
+        self.assertEqual(10.75, wallet)
 
