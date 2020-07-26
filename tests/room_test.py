45803037da9527ae12e024b488870da545c608d6
Duncan Ryan <duncancryan1@gmail.com>
2020-07-27 00:08:58 +0100

    test passed

--- tests/room_test.py
+++ tests/room_test.py
@@ -8,10 +8,10 @@ class TestRoom(unittest.TestCase):
         self.room_01 = Room("The Party Room", 4)
         self.room_02 = Room("The Date Room", 2)
         
-        self.guest_01 = Guest("Frodo Baggins", 51)
-        self.guest_02 = Guest("Samwise Gamgee", 36)
-        self.guest_03 = Guest("Meriadoc Brandybuck", 37)
-        self.guest_04 = Guest("Peregrin Took", 29)
+        self.guest_01 = Guest("Frodo Baggins", 51, 30.50)
+        self.guest_02 = Guest("Samwise Gamgee", 36, 10.75)
+        self.guest_03 = Guest("Meriadoc Brandybuck", 37, 23.60)
+        self.guest_04 = Guest("Peregrin Took", 29, 17.85)
 
         self.song_01 = Song("Come on Eileen", "Dexys Midnight Runners")
         self.song_02 = Song("Mr Blue Sky", "ELO")
