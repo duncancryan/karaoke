db75a3c5c31b16e909d8ceba130ab9a2ea59b9ea
Duncan Ryan <duncancryan1@gmail.com>
2020-07-26 22:45:42 +0100

    room class defined, tests passing

--- tests/room_test.py
+++ tests/room_test.py
@@ -5,15 +5,15 @@ from classes.song import Song
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        room_01 = Room("The Party Room")
-        room_02 = Room("The Date Room")
+        self.room_01 = Room("The Party Room")
+        self.room_02 = Room("The Date Room")
 
     def test_room_has_name(self):
         name = self.room_01.name
         self.assertEqual("The Party Room", name)
 
     def test_room_pax_starts_at_0(self):
-        pax = len(self.room_01.pax)
+        pax = len(self.room_01.guests)
         self.assertEqual(0, pax)
 
     def test_room_songs_start_at_0(self):
