8a2f2599d5e44414ee7aab0aaa2a2a722aee05eb
Duncan Ryan <duncancryan1@gmail.com>
2020-07-26 22:42:46 +0100

    property tests for room class written

--- tests/room_test.py
+++ tests/room_test.py
@@ -3,4 +3,19 @@ from classes.room import Room
 from classes.guest import Guest
 from classes.song import Song
 
-class TestRoom(unittest.TestCase)
+class TestRoom(unittest.TestCase):
+    def setUp(self):
+        room_01 = Room("The Party Room")
+        room_02 = Room("The Date Room")
+
+    def test_room_has_name(self):
+        name = self.room_01.name
+        self.assertEqual("The Party Room", name)
+
+    def test_room_pax_starts_at_0(self):
+        pax = len(self.room_01.pax)
+        self.assertEqual(0, pax)
+
+    def test_room_songs_start_at_0(self):
+        songs = len(self.room_01.songs)
+        self.assertEqual(0, songs)
