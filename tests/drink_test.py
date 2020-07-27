b07904e20f96f407bdfedb14276c8627a1e17c41
Duncan Ryan <duncancryan1@gmail.com>
2020-07-27 01:29:08 +0100

    property tests for drinks class written

--- tests/drink_test.py
+++ tests/drink_test.py
@@ -1,2 +1,19 @@
 import unittest
 from classes.drink import Drink
+
+class TestDrink(unittest.TestCase):
+    def setUp(self):
+        self.drink_01 = Drink("Gaffer's Home Brew", 5)
+        self.drink_02 = Drink("Dry Martini", 2)
+        self.drink_03 = Drink("Margarita", 2)
+        self.drink_04 = Drink("Manhattan", 2)
+        self.drink_05 = Drink("Sazerac", 3)
+        self.drink_06 = Drink("Highball", 1)
+
+    def test_drink_has_name(self):
+        name = self.drink_01.name
+        self.assertEqual("Gaffer's Home Brew", name)
+
+    def test_drink_has_alcohol_level(self):
+        abv = self.drink_02.alcohol
+        self.assertEqual(2, abv)
